f5ca1bc7d7b0db963c5129c0d5a8465ef685c58a
Roboto01100111 <tucorreo@ejemplo.com>
2025-06-18 02:05:55 -0500

    Proyecto final corregido - laboratorio 9 Django

--- pokedex/models.py
+++ pokedex/models.py
@@ -14,14 +14,8 @@ class Pokemon(models.Model):
     weight = models.IntegerField(null=False)
     height = models.IntegerField(null=False)
     picture = models.ImageField(upload_to="pokemon_images", null=True)
-    entrenador = models.CharField(max_length=100, null=True)
-
-    # Campos extra de la otra rama (opcional incluirlos si se usan)
-    nombre = models.CharField(max_length=100, null=True, blank=True)
-    tipo = models.CharField(max_length=50, null=True, blank=True)
-    nivel = models.IntegerField(default=1)
-    descripcion = models.TextField(null=True, blank=True)
-    imagen = models.ImageField(upload_to='imagenes_pokemon/', null=True, blank=True)
+    
+    entrenador = models.ForeignKey(Trainer, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
-        return self.name or self.nombre or "Pokemon"
+        return self.name
